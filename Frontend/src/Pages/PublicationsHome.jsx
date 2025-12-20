23ba53ff0089a2a108be2540026e4cc4629b3d94
Adis-ore <adisoreoluwa@gmail.com>
2025-12-20 18:55:29 +0100

    Creation of the home pages

--- Frontend/src/Pages/PublicationsHome.jsx
+++ Frontend/src/Pages/PublicationsHome.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const App = () => {
+const PublicationsHome = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const App = () => {
   )
 }
 
-export default App
+export default PublicationsHome
