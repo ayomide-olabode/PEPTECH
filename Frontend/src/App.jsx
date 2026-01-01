520432ca7942731e10600aad1e774e1b462cd728
okikiareo <okikiareo@gmail.com>
2026-01-01 14:01:12 +0100

    conflict resolve

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { Routes, Route } from 'react-router-dom'
+import React from "react";
+import Navbar from "./Components/Navbar";
 import Footer from "./Components/footer/Footer";
 import Home from "./Pages/Hero";
+import OurDepartment from "./Pages/OurDepartment";
 import Publications from "./Pages/publications/publications";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import "./index.css";
 
 const App = () => {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/publications" element={<Publications />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/our-department" element={<OurDepartment />} />
+       <Route path="/publications" element={<Publications />} />
+      </Routes>
     <Footer/>
-  </Router>
-  )
+    </Router>
+  );
 };
 
-export default App
+export default App;
